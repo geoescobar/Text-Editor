47379cf66ebf2c3c837bcd81be54675ae1845ad4
George Escobar <geo.escobar214@gmail.com>
2022-06-30 20:55:53 -0500

    first commit

---
+++ notes.js
@@ -0,0 +1,16 @@
+//JP Morgan interview questions 
+
+// 1. 
+const arr = [10, 12, 15, 21];
+for (let i = 0; i < arr.length; i++) {
+  setTimeout(() => {
+    console.log("Index" + i + ", element" + arr[i]);
+  }, 3000);
+}
+
+// 2.
+// What is a closure?
+
+
+
+
